a22ed74161f10ab3b5f96e0512966c68a0b8cb51
gagaruk <red8gams@gmail.com>
2026-05-11 21:51:13 +0300

    pin assignment

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,23 @@
 #include <Arduino.h>
 
+#define A1PIN 7
+#define A2PIN 8
+#define B1PIN 5
+#define B2PIN 4
+#define STBY 6
+#define PWMA 9
+#define PWMB 3
+
+void forward(int);
+
+void setup() {
+
+  pinMode(STBY, OUTPUT);
+  pinMode(A1PIN, OUTPUT);
+  pinMode(A2PIN, OUTPUT);
+  pinMode(B1PIN, OUTPUT);
+  pinMode(B2PIN, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+
+}
